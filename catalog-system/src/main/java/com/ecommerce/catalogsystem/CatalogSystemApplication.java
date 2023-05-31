7901fdbc75efdfe0276bc53da244c54329a91ecc
Suhailah Moraes <suh.alvesmoraes@gmail.com>
2023-05-31 01:19:55 -0300

    Created Catalog-System Project

---
+++ catalog-system/src/main/java/com/ecommerce/catalogsystem/CatalogSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.catalogsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogSystemApplication.class, args);
+	}
+
+}
---
+++ catalog-system/src/test/java/com/ecommerce/catalogsystem/CatalogSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.catalogsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
