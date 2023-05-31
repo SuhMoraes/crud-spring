d9adbfac64f0ed1a767815d6e78e93fb18d7429c
Suhailah Moraes <suh.alvesmoraes@gmail.com>
2023-05-31 01:42:22 -0300

    Category Resource

---
+++ catalog-system/src/main/java/com/ecommerce/catalogsystem/domain/Category.java
@@ -0,0 +1,46 @@
+package com.ecommerce.catalogsystem.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable {
+    private Long id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id) && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
