6b7d9413539d6e83c4a42e7c2b36852c599cc655
Suhailah Moraes <suh.alvesmoraes@gmail.com>
2023-08-10 17:02:33 -0300

    Project Created

---
+++ java-spring-expert/src/main/java/com/suhmoraes/javaspringexpert/JavaSpringExpertApplication.java
@@ -0,0 +1,13 @@
+package com.suhmoraes.javaspringexpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringExpertApplication.class, args);
+	}
+
+}
---
+++ java-spring-expert/src/test/java/com/suhmoraes/javaspringexpert/JavaSpringExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhmoraes.javaspringexpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringExpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
